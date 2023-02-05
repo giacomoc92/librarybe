529f02499f09cd0e4a188f730e64bc61561387e5
giacomoc92 <consani.gia@gmail.com>
2023-02-05 15:54:16 +0100

    initial commit

---
+++ src/main/java/it/hastega/librarybe/librarybe/LibrarybeApplication.java
@@ -0,0 +1,13 @@
+package it.hastega.librarybe.librarybe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarybeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarybeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/hastega/librarybe/librarybe/LibrarybeApplicationTests.java
@@ -0,0 +1,13 @@
+package it.hastega.librarybe.librarybe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarybeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
